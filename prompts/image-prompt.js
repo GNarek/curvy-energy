6c824b8e874a0337c14fd5cd7629a78bbf0ff00e
Narek Ghazaryan <narekghazaryandev@gmail.com>
2025-04-21 23:33:54 +0400

    Change group probability

--- prompts/image-prompt.js
+++ prompts/image-prompt.js
@@ -134,7 +134,7 @@ const generateDallePrompt = () => {
     'early morning light on full body',
   ];
 
-  const isGroup = Math.random() > 0.3;
+  const isGroup = Math.random() < 0.2;
 
   const mainAction = isGroup ? random(groupPoses) : random(poses);
   const location = random(locations);
